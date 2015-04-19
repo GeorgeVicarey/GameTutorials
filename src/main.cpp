975d19d39d4a8646177d3879a09227ba91863020
GeorgeVicarey <g.vicarey@hotmail.co.uk>
2015-04-19 12:51:12 +0100

    Added Hello world

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 19 Apr 2015
+ *      Author: George
+ */
+
+ #include <iostream>
+
+ int main()
+ {
+	std::cout << "Hello, world!" << std::endl;
+
+	return 0;
+ }
